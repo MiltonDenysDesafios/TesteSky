4cec9faf7c006843c74f817f6c81b2c2c3759519
Milton Denys <milton.silva17@fatec.sp.gov.br>
2020-10-13 22:23:15 -0300

    update
    validar ainda modal com bug e fazer parte final do teste

--- src/test/java/funcionalidades/ValidarModalFuncionalidades.java
+++ src/test/java/funcionalidades/ValidarModalFuncionalidades.java
@@ -25,21 +25,33 @@ public class ValidarModalFuncionalidades extends Comandos{
 		//posiciona na palavra programacao
 		posicionaNaPalavraProgramacao();
 
-
 		
 	}
-	
+	/**
+	 * @author Milton
+	 * @return
+	 * fechar primeiro banner 
+	 */
 	public static void fecharBannerBemVindo() {
 		
 		clicar("//*[@id=\"portlet_com_liferay_journal_content_web_portlet_JournalContentPortlet_INSTANCE_YQcCxObpxg0J\"]/div/div[2]/div/div[2]/div/div/div/div[1]/button");
 		
 	}
+	/**
+	 * @author Milton
+	 * @return
+	 * fechar segundo banner de cookies
+	 */
 	public static void fecharBannerOkEntendi() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		clicar("//*[@id=\"wrap-aviso-cookie\"]/div[1]/div/div/div/div[3]/div/div/button");
 		
 	}
-	
+	/**
+	 * @author Milton
+	 * @return
+	 * metodo para posicionar a tela no ponto mapeado
+	 */
 	public static void posicionaNaPalavraProgramacao() {
 		
 		posicaoDaTela("//*[@id=\"portlet_com_liferay_journal_content_web_portlet_JournalContentPortlet_INSTANCE_aKJUz209eKa8\"]/div/div[2]/div/div[2]/section/div/div[1]/div/div/a[1]/h2");
